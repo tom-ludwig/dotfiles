f2f8b5580ef9bf612ffe401ed1b41efa7493ced4
Tom Ludwig <tommludwig@icloud.com>
2024-09-21 19:56:13 +0200

    Update vim notify to use compact style

--- .config/nvim/lua/hero/plugins/nvim-notify.lua
+++ .config/nvim/lua/hero/plugins/nvim-notify.lua
@@ -2,6 +2,13 @@ return {
   "rcarriga/nvim-notify",
   config = function()
     local notify = require("notify")
+    notify.setup({
+      stages = "static",
+      render = "compact",
+      background_colour = "FloatShadow",
+      timeout = 3000,
+    })
+
     -- Set the default notification system to use notify
     vim.notify = notify
   end,
