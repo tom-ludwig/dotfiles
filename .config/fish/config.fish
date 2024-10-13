fa10178f3cb525d75b9cc2ca6910f9f8f1f5b49e
= <tommludwig@icloud.com>
2024-10-13 20:24:20 +0200

    Moved to fish shell

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,6 +4,32 @@ if status is-interactive
     starship init fish | source
 end
 
+# +---------+
+# | General |
+# +---------+
+alias fuck='thefuck'
+
+# +---------+
+# |   GIT   |
+# +---------+
+alias gs='git status'
+alias ga='git add'
+alias gp='git push'
+alias gpo='git push origin'
+alias gtd='git tag --delete'
+alias gtdr='git tag --delete origin'
+alias gr='git branch -r'
+alias gplo='git pull origin'
+alias gb='git branch '
+alias gc='git commit'
+alias gd='git diff'
+alias gco='git checkout '
+alias gl='git log'
+alias gr='git remote'
+alias grs='git remote show'
+alias glo='git log --pretty="oneline"'
+alias glol='git log --graph --oneline --decorate'
+
 # List Directory
 alias ls="lsd"
 alias l="ls -l"
@@ -20,3 +46,5 @@ abbr .5 'cd ../../../../..'
 
 # Always mkdir a path (this doesn't inhibit functionality to make a single dir)
 abbr mkdir 'mkdir -p'
+
+neofetch
